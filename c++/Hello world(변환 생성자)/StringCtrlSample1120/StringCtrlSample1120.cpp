05ae70d2bad8458056979db2f3eea422383d3c86
AHYEON04 <201120cay@dsm.hs.kr>
2020-10-08 20:07:01 +0900

    10.08

---
+++ c++/Hello world(변환 생성자)/StringCtrlSample1120/StringCtrlSample1120.cpp
@@ -0,0 +1,14 @@
+﻿#include	<iostream>
+#include	"CMyString.h"
+using	namespace	std;
+void	TestFunc(const	CMyString& strParam)
+{
+	cout << strParam << endl;
+}
+int	main()
+{
+	CMyString	strData("Hello");
+	::TestFunc(strData);
+	::TestFunc(CMyString("World"));
+	return	0;
+}
